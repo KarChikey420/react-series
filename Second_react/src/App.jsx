bae1903c7f1a0f58b73511c1330f43aeec1cc638
KarChikey <kartikeynegi2002@gmail.com>
2026-01-24 23:52:48 +0530

    some changes in app

--- Second_react/src/App.jsx
+++ Second_react/src/App.jsx
@@ -5,16 +5,32 @@ function App() {
   let [count, setCount] = useState(0)
   
   const addValue=() => {
-    console.log("Button clicked", Math.random());
-    count=count+1
-    setCount(count)
+    // console.log("Button clicked", Math.random());
+    if (count<20){
+        count=count+1
+        setCount(count)
+    }else{
+      console.log('stop there !')
+    }
+  }
+
+  const removeValue=()=>{
+    if(count>0){
+        count=count-1
+        setCount(count)
+    }else{
+      console.log('stop there!')
+    }
   }
 
   return (
     <>
+    <div>
     <h1>Add a Number</h1>
     <h2>Number is {count}</h2>
     <button onClick={addValue}>Add</button>
+    <button onClick={removeValue}>Remove</button>
+    </div>
     </>
   )
 }
