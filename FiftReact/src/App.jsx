99b28d8b609809e787b57a6338ab9e470076533c
KarChikey <kartikeynegi2002@gmail.com>
2026-01-30 00:19:02 +0530

    add

--- FiftReact/src/App.jsx
+++ FiftReact/src/App.jsx
@@ -1,24 +1,27 @@
+import { Fragment } from 'react'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[title,SetTitle]=useState("my name is kartikey")
 
-  function 
-
-  return (
-    <>
-      <buttoon onclick={ClickButton}>Click me!</buttoon>
-      <Header title="Welcome To My Arena"/>
-      <Header title="Whats Going On"/>
-    </>
+  function ClickButton(){
+      SetTitle("Hey! my name is"+ " "+Math.random())
+  }
+   
+  return(
+    <Fragment>
+     <button onClick={ClickButton}>Click Me</button>
+     <Header title={title}/>
+    </Fragment>
   )
 }
-  function Header({title}){
-     return <div>
+
+function Header({title}){
+  return(
+    <div>
       <h1>{title}</h1>
-     </div>
-  }
-
-
+    </div>
+  )
+}
 export default App
