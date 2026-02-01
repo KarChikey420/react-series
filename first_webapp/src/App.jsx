82e73096e999eb2a384a7ea061c9d1e1e33a53b4
KarChikey <kartikeynegi2002@gmail.com>
2026-02-02 01:44:12 +0530

    add

--- first_webapp/src/App.jsx
+++ first_webapp/src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import MyApp from './myapp'
+import { useContext, useState } from 'react'
+import { CountContext } from './context'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <MyApp/>
-      <p>Hi Kartikey!</p>
+      <CountContext.Provider value={{count, setCount}}>
+        <Count />
+      </CountContext.Provider>
     </div>
   )
+}
+
+function Count(){
+    return <div>
+      <CountRenderer/>
+      <Button />
+    </div>
+}
+
+function CountRenderer(){
+  const {count} = useContext(CountContext);
+  return <div>
+    {count}
+  </div>
+}
+
+function Button(){
+  const {count, setCount} = useContext(CountContext);
+  return <div>
+    <button onClick={()=>{
+        setCount(count+1)
+    }}>Increase</button>
+
+    <button onClick={()=>{
+        setCount(count-1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
