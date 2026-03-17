cb33f5df398fdf82a18d8273d5ebba6aa7c3af99
KarChikey <kartikeynegi2002@gmail.com>
2026-03-17 19:51:43 +0530

    component in same file

--- Practice_of_react/vite-project/src/App.jsx
+++ Practice_of_react/vite-project/src/App.jsx
@@ -1,11 +1,18 @@
-import Header from "./header";
 function Apple(){
     return(
       <div>
         <Header />
-        <h1 style={{color:"blue"}}>Buy A Sweet Apple</h1>
+        <h1 style={{color:"red"}}>Apple</h1>
       </div>
     )
+}
+
+function Header(){
+      return(
+        <div>
+          <h1>Welcome my Fruit App</h1>
+        </div>
+      )
 }
 
 export default Apple;
