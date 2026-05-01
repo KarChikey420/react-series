b81e5b76e6836c2537ec802028dac8bb865545a4
KarChikey <kartikeynegi2002@gmail.com>
2026-05-01 23:03:20 +0530

    add

--- basic_practice_of_react/todo-list/src/App.jsx
+++ basic_practice_of_react/todo-list/src/App.jsx
@@ -15,7 +15,17 @@ export default function App(){
       }
 
       return <div>
+        <input value={task} onChange={(e)=> setTask(e.targetvalue)}/>
+        <button onClick={addTask}>Add</button>
 
+        <ul>
+        {todo.map((t, i) => (
+          <li key={i}>
+            {t}
+            <button onClick={() => deleteTask(i)}>❌</button>
+          </li>
+        ))}
+      </ul>
       </div>
 
 
