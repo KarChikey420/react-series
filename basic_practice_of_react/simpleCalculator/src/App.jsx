1a20ef96d0f2e96e484ffd462402c4b1a7364f3f
KarChikey <kartikeynegi2002@gmail.com>
2026-05-06 22:30:26 +0530

    create calculator

---
+++ basic_practice_of_react/simpleCalculator/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export default function App(){
+  const [num1, setNum1]=useState(0)
+  const [num2, setNum2]=useState(0)
+  const [result, setResult]=useState(0)
+
+  const handleAddition=()=>{
+     setResult(Number(num1)+Number(num2))
+  }
+  const handleSubtraction=()=>{
+    setResult(Number(num1)-Number(num2))
+  }
+  const handleMultiplication=()=>{
+    setResult(Number(num1)*Number(num2))
+  }
+  const handleDivision=()=>{
+    if(num2!==0){
+      setResult(Number(num1)/Number(num2))
+    }else{
+      setResult("Cannot divide by zero")
+    }
+  }
+}
